e118b6227376bc98995a606a393d4149abb10b09
DayraJr <dayajimenezr@udistrital.edu.co>
2026-05-20 11:32:27 -0500

    Prueba

--- aplicativos-conductimetro/Control-dos-conductímetros/__init__.py
+++
@@ -1,2 +0,0 @@
-# Paquete Aplicación - Conductimetro
-__version__ = "1.0.0"
--- aplicativos-conductimetro/Control-dos-conductímetros/__main__.py
+++
@@ -1,4 +0,0 @@
-# Punto de entrada para ejecutar el paquete como módulo
-if __name__ == '__main__':
-    from .main import main
-    main()
